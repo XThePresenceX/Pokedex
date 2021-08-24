18c1d0ae9c6e292dbf03c962fee9f33ddf5a4a9d
XThePresenceX <jensoni96@gmail.com>
2021-08-24 14:46:10 -0400

    Initial Author Routes

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const internalRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser: true, useUnifiedTopology: true});
 
@@ -22,8 +24,10 @@ app.set('views',__dirname+'/views')
 app.set('layout','layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit:'10mb', extended:false}))
 
 app.use('/',internalRouter)
+app.use('/authors', authorRouter)
 
 
 app.listen(process.env.PORT||3000)
